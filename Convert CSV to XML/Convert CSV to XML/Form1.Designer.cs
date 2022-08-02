a7fe0976d9e93100499915e6fb3567c20f385f42
danuts <matei1337@icloud.com>
2022-08-02 15:10:53 +0300

    first version of the winform application released.

---
+++ Convert CSV to XML/Convert CSV to XML/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Convert_CSV_to_XML
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePath = new System.Windows.Forms.Label();
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.btUpload = new System.Windows.Forms.Button();
+            this.csvPath = new System.Windows.Forms.TextBox();
+            this.csvGridView = new System.Windows.Forms.DataGridView();
+            this.exportXML = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.csvGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // filePath
+            // 
+            this.filePath.AutoSize = true;
+            this.filePath.Location = new System.Drawing.Point(27, 60);
+            this.filePath.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(55, 15);
+            this.filePath.TabIndex = 0;
+            this.filePath.Text = "File path:";
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(499, 58);
+            this.btBrowse.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(118, 27);
+            this.btBrowse.TabIndex = 1;
+            this.btBrowse.Text = "Browse";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // btUpload
+            // 
+            this.btUpload.Location = new System.Drawing.Point(89, 88);
+            this.btUpload.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btUpload.Name = "btUpload";
+            this.btUpload.Size = new System.Drawing.Size(118, 27);
+            this.btUpload.TabIndex = 2;
+            this.btUpload.Text = "Upload CSV";
+            this.btUpload.UseVisualStyleBackColor = true;
+            this.btUpload.Click += new System.EventHandler(this.btUpload_Click);
+            // 
+            // csvPath
+            // 
+            this.csvPath.Location = new System.Drawing.Point(89, 58);
+            this.csvPath.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.csvPath.Name = "csvPath";
+            this.csvPath.Size = new System.Drawing.Size(402, 23);
+            this.csvPath.TabIndex = 3;
+            // 
+            // csvGridView
+            // 
+            this.csvGridView.AllowUserToAddRows = false;
+            this.csvGridView.AllowUserToDeleteRows = false;
+            this.csvGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.csvGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.csvGridView.Location = new System.Drawing.Point(12, 129);
+            this.csvGridView.Name = "csvGridView";
+            this.csvGridView.ReadOnly = true;
+            this.csvGridView.Size = new System.Drawing.Size(999, 492);
+            this.csvGridView.TabIndex = 4;
+            // 
+            // exportXML
+            // 
+            this.exportXML.Location = new System.Drawing.Point(215, 88);
+            this.exportXML.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.exportXML.Name = "exportXML";
+            this.exportXML.Size = new System.Drawing.Size(118, 27);
+            this.exportXML.TabIndex = 5;
+            this.exportXML.Text = "Export to XML";
+            this.exportXML.UseVisualStyleBackColor = true;
+            this.exportXML.Click += new System.EventHandler(this.btExport_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(380, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 643);
+            this.Controls.Add(this.exportXML);
+            this.Controls.Add(this.csvGridView);
+            this.Controls.Add(this.csvPath);
+            this.Controls.Add(this.btUpload);
+            this.Controls.Add(this.btBrowse);
+            this.Controls.Add(this.filePath);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Convert CSV to XML";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.csvGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label filePath;
+        private Button btBrowse;
+        private Button btUpload;
+        private TextBox csvPath;
+        private DataGridView csvGridView;
+        private Button exportXML;
+        private Button button1;
+    }
+}
---
+++ Convert CSV to XML/Convert CSV to XML/Form1.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Convert_CSV_to_XML
+{ 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            // To list only csv files, we need to add this filter
+            openFileDialog.Filter = "|*.csv";
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                csvPath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                csvGridView.DataSource = GetDataTableFromCSVFile(csvPath.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Import CSV File", MessageBoxButtons.OK,
+  MessageBoxIcon.Error);
+            }
+        }
+
+        private void btExport_Click(object sender, EventArgs e) {
+            try
+            {
+                DataTable dt = (DataTable)csvGridView.DataSource;
+                dt.TableName = "CSV";
+                dt.WriteXml(@"C:\Users\Public\CSV.xml");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Export CSV File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static DataTable GetDataTableFromCSVFile(string csvfilePath)
+        {
+            DataTable csvData = new();
+            using (TextFieldParser csvReader = new(csvfilePath))
+            {
+                csvReader.SetDelimiters(new string[] { "," });
+                csvReader.HasFieldsEnclosedInQuotes = true;
+
+                //Read columns from CSV file, remove this line if columns not exits  
+                string[] colFields = csvReader.ReadFields();
+
+                foreach(string column in colFields)
+                {
+                    if (column == "Number" || column == "Status" || column == "Createddate" || column == "Duedate" || column == "Total" || column == "Taxes" || column == "Discount" || column == "Amountpaid" || column == "Amountdue" || column == "Organizationname" || column == "Clientname" || column == "Organizationaddress" || column == "Clientaddress" || column == "Organizationregistrationnumber" || column == "Organizationtaxid")
+                    {
+                        DataColumn datecolumn = new DataColumn(column);
+                        datecolumn.AllowDBNull = true;
+                        csvData.Columns.Add(datecolumn);
+                    }
+                }
+
+                while (!csvReader.EndOfData)
+                {
+                    string[] fieldData = csvReader.ReadFields();
+                    //Making empty value as null
+                    for (int i = 0; i < fieldData.Length; i++)
+                    {
+                        if (fieldData[i] == "")
+                        {
+                            fieldData[i] = null;
+                        }
+                    }
+                    csvData.Rows.Add(fieldData);
+                }
+            }
+            return csvData;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
