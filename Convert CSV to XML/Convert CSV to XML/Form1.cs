eb2fc1c5ae6ef6e58f1a0d547d5751af95a8ab92
danuts <matei1337@icloud.com>
2022-08-02 20:23:18 +0300

    csv to xml file with all needed elements.

--- Convert CSV to XML/Convert CSV to XML/Form1.cs
+++ Convert CSV to XML/Convert CSV to XML/Form1.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
 using Microsoft.VisualBasic.FileIO;
 
 namespace Convert_CSV_to_XML
 { 
     public partial class Form1 : Form
     {
+        DataSet ds = new DataSet();
         public Form1()
         {
             InitializeComponent();
@@ -45,12 +49,94 @@ namespace Convert_CSV_to_XML
             }
         }
 
-        private void btExport_Click(object sender, EventArgs e) {
+        private void btExport_Click(object sender, EventArgs e)
+        {
             try
             {
-                DataTable dt = (DataTable)csvGridView.DataSource;
-                dt.TableName = "Facturi";
-                dt.WriteXml(@"C:\Users\Public\CSV.xml");
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "XML|*.xml";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        DataTable dt = (DataTable)csvGridView.DataSource;
+                        // dt.TableName = "Facturi";
+                        // dt.WriteXml(sfd.FileName);
+
+                        XElement xe = new XElement("Facturi",
+                            from row in dt.AsEnumerable()
+                            select new XElement("Factura",
+                                    new XElement("Antet",
+                                        new XElement(("FurnizorNume") ?? "ZERO SAPTE SERVICES S.R.L"),
+                                        new XElement(("FurnizorCIF") ?? "45858226"),
+                                        new XElement(("FurnizorNrRegCom") ?? "J13/1003/2022"),
+                                        new XElement(("FurnizorCapital") ?? "200 RON"),
+                                        new XElement(("FurnizorTara") ?? "Romania"),
+                                        new XElement(("FurnizorLocalitate") ?? "Navodari"),
+                                        new XElement(("FurnizorJudet") ?? "Constanta"),
+                                        new XElement(("FurnizorAdresa"), row.Field<string>("Organization address")),
+                                        new XElement(("FurnizorTelefon" ?? "")),
+                                        new XElement(("FurnizorMail" ?? "client@07internet.ro")),
+                                        new XElement(("FurnizorBanca" ?? "")),
+                                        new XElement(("FurnizorIBAN" ?? "")),
+                                        new XElement(("FurnizorInformatiiSuplimentare"),
+                                            new XElement(("GUID_cod_client") ?? "")),
+                                        new XElement(("ClientNume"), row.Field<string>("Client name")),
+                                        new XElement(("ClientInformatiiSuplimentare") ?? ""),
+                                        new XElement(("ClientCIF") ?? ""),
+                                        new XElement(("ClientNrRegCom") ?? ""),
+                                        new XElement(("ClientJudet") ?? ""),
+                                        new XElement(("ClientTara") ?? "RO"),
+                                        new XElement(("ClientLocalitate") ?? ""),
+                                        new XElement(("ClientAdresa"), row.Field<string>("Client address")),
+                                        new XElement(("ClientBanca") ?? ""),
+                                        new XElement(("ClientIBAN") ?? ""),
+                                        new XElement(("ClientTelefon") ?? ""),
+                                        new XElement(("ClientMail") ?? ""),
+                                        new XElement(("FacturaNumar"), row.Field<string>("Number")),
+                                        new XElement(("FacturaData"), row.Field<string>("Created date")),
+                                        new XElement(("FacturaScadenta"), row.Field<string>("Due date")),
+                                        new XElement(("FacturaTaxareInversa") ?? ""),
+                                        new XElement(("FacturaTVAIncasare") ?? ""),
+                                        new XElement(("FacturaTip") ?? ""),
+                                        new XElement(("FacturaInformatiiSuplimentare"),
+                                            new XElement(("FacturaMoneda") ?? "RON")),
+                                        new XElement(("FacturaGreutate") ?? ""),
+                                        new XElement(("FacturaAccize") ?? ""),
+                                        new XElement(("Cod") ?? "")),
+                                    new XElement("Detalii",
+                                        new XElement("Continut",
+                                            new XElement("Linie",
+                                                new XElement(("LinieNrCrt") ?? ""),
+                                                new XElement(("Gestiune") ?? ""),
+                                                new XElement(("Activitate") ?? ""),
+                                                new XElement(("Descriere") ?? ""),
+                                                new XElement(("CodArticolFurnizor") ?? ""),
+                                                new XElement("CodArticolClient",
+                                                    new XElement(("GUID_cod_articol") ?? ""),
+                                                    new XElement(("CodBare") ?? "")),
+                                                new XElement(("InformatiiSuplimentare") ?? ""),
+                                                new XElement(("UM") ?? ""),
+                                                new XElement(("Cantitate") ?? ""),
+                                                new XElement("Pret", row.Field<string>("Total")),
+                                                new XElement("Valoare", row.Field<string>("Total")),
+                                                new XElement(("ProcTVA") ?? ""),
+                                                new XElement(("TVA") ?? ""),
+                                                new XElement(("Cont") ?? "",
+                                                    new XElement(("TipDeducere") ?? ""),
+                                                    new XElement(("PretVanzare") ?? ""))))),
+                                    new XElement(("FacturaID") ?? "")));
+                        xe.Save(sfd.FileName);
+
+                        MessageBox.Show("File saved successfully", "Export to XML", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                    }
+                    catch (Exception error)
+                    {
+                        MessageBox.Show(error.Message, "Export CSV File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                }
             }
             catch (Exception ex)
             {
